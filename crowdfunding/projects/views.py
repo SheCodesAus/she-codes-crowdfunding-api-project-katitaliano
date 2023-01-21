494ce75907c6fcb0f06aa1792e3dcdb5af189f78
kat.italiano@gmail.com <kat.italiano>
2023-01-21 11:49:48 +0800

    Permissions and token authorisation

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
 
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+from .permissions import IsOwnerOrReadOnly
 
 class ProjectList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def get(self, request):
         projects = Project.objects.all()
@@ -25,14 +27,31 @@ class ProjectDetail(APIView):
 
     def get_object(self, pk):
         try:
-            return Project.objects.get(pk=pk)
+            project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request, project)
+            return project
         except Project.DoesNotExist:
             raise Http404
+
 
     def get(self, request, pk):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+          project = self.get_object(pk)
+          data = request.data
+          serializer = ProjectDetailSerializer(
+               instance=project,
+               data=data,
+                 partial=True
+          )
+          if serializer.is_valid():
+               serializer.save()
+               return Response(serializer.data)
+          return Response(serializer.errors)
+
 
 class PledgeList(generics.ListCreateAPIView):
     queryset = Pledge.objects.all()
